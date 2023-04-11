24be8b1a846f6db6e9aafd6dee72ee1a8c6aa876
Vincent Pastorino <vpastori@DESKTOP-TDG3FUD>
2023-04-10 20:45:01 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  const paragraph = document.querySelector('p');
+  paragraph.textContent = 'This is really cool!';
+});
